d4d7836be0dfc9164ce4fc99dfb18a68236bf567
julian <julian.gio.wieckardt@gmail.com>
2019-04-13 15:34:30 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+    <head>
+      <title>website title</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Free Web tutorials">
+      <meta name="keywords" content="HTML, CSS, XML, JavaScript">
+      <meta name="author" content="Julian Wieckardt">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+      <nav>
+        <ul>
+          <li><a>
+        </ul>
+        <nav>
+
+
+    </body>
+
+</html>
